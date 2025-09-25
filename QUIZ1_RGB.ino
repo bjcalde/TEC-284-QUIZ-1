ea9cf25f5aca290dd5e957af979fab2f86e63ed2
bjcalde <157441710+bjcalde@users.noreply.github.com>
2025-09-25 14:25:19 -0500

    2nd Commit
    2nd commit, lights now turn on when corresponding button is pressed.

--- QUIZ1_RGB.ino
+++ QUIZ1_RGB.ino
@@ -1,9 +1,12 @@
 void setup() {
   // put your setup code here, to run once:
-  
+  // Using pin 7(R), 6(G) and 5(B) for the buttons. When each button is pressed, the corresponding color will be turned on.
+  pinMode(7, INPUT_PULLUP);
+  pinMode(6, INPUT_PULLUP);
+  pinMode(5, INPUT_PULLUP);
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
 }
